7abb4c43941dd4a528c4afd0c262276ff8b504d1
devarend <devarend@gmail.com>
2024-10-17 10:52:34 +0200

    changed favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+          <link rel="icon" href="/icon.svg" sizes="any" />
+      </Head>
       <body className="dark text-foreground bg-background">
         <Main />
         <NextScript />
